a0f8bbbdc231c8e950fc878b815f89fe6744f447
Flywith24 <fly_with24@163.com>
2020-06-25 19:27:14 +0800

    add color extension

---
+++ app/src/main/java/com/flywith24/flywith24_permissiondemo/ColorExt.kt
@@ -0,0 +1,25 @@
+package com.flywith24.flywith24_permissiondemo
+
+import android.content.Context
+import android.graphics.Color
+import androidx.annotation.AttrRes
+import androidx.annotation.ColorInt
+import androidx.core.content.res.use
+
+/**
+ * @author Flywith24
+ * @date   2020/6/25
+ * time   19:22
+ * description
+ * 颜色扩展
+ */
+
+/**
+ * 根据主题属性获取颜色
+ */
+@ColorInt
+fun Context.getThemeColor(@AttrRes themeAttrId: Int): Int {
+    return obtainStyledAttributes(intArrayOf(themeAttrId)).use {
+        it.getColor(0, Color.MAGENTA)
+    }
+}
