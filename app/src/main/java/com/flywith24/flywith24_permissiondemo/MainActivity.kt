2eb06fdedcbd42b48aed1ad6217a0863f0b6e494
Flywith24 <fly_with24@163.com>
2020-06-25 19:35:41 +0800

    show result text

--- app/src/main/java/com/flywith24/flywith24_permissiondemo/MainActivity.kt
+++ app/src/main/java/com/flywith24/flywith24_permissiondemo/MainActivity.kt
@@ -4,17 +4,30 @@ import android.Manifest
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     fun click(view: View) {
         requestMultiplePermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA),
                 allGranted = {
-                    Snackbar.make(view, "all granted", Snackbar.LENGTH_SHORT).show()
+                    Snackbar.make(view, getString(R.string.result_granted_list), Snackbar.LENGTH_SHORT).show()
                 },
                 denied = {
+                    //防止太长体验不好，真实使用时无需做此操作
                     val result = it.toString().replace("android.permission.", "")
-                    Snackbar.make(view, "未通过权限列表：\n$result", Snackbar.LENGTH_SHORT).show()
+                    with(deniedResult) {
+                        text = getString(R.string.result_denied_list, result)
+                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                    }
+                },
+                explained = {
+                    //防止太长体验不好，真实使用时无需做此操作
+                    val result = it.toString().replace("android.permission.", "")
+                    with(explainedResult) {
+                        text = getString(R.string.result_explained_list, result)
+                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                    }
                 })
     }
 }
--- app/src/main/java/com/flywith24/flywith24_permissiondemo/MainFragment.kt
+++ app/src/main/java/com/flywith24/flywith24_permissiondemo/MainFragment.kt
@@ -4,7 +4,6 @@ import android.Manifest
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.fragment_main.*
 
 /**
@@ -17,14 +16,29 @@ class MainFragment : Fragment(R.layout.fragment_main) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         requestPermission.setOnClickListener {
             requestPermission(Manifest.permission.RECORD_AUDIO,
-                granted = {
-                    result.text = getString(R.string.result_granted)
+                granted = { permission ->
+                    //防止太长体验不好，真实使用时无需做此操作
+                    val result = permission.replace("android.permission.", "")
+                    with(tvResult) {
+                        text = getString(R.string.result_granted, result)
+                        setTextColor(context.getThemeColor(R.attr.colorSurface))
+                    }
                 },
-                denied = {
-                    result.text = getString(R.string.result_denied)
+                denied = { permission ->
+                    //防止太长体验不好，真实使用时无需做此操作
+                    val result = permission.replace("android.permission.", "")
+                    with(tvResult) {
+                        text = getString(R.string.result_denied, result)
+                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                    }
                 },
-                explained = {
-                    Snackbar.make(view, "勾选不再提示", Snackbar.LENGTH_SHORT).show()
+                explained = { permission ->
+                    //防止太长体验不好，真实使用时无需做此操作
+                    val result = permission.replace("android.permission.", "")
+                    with(tvResult) {
+                        text = getString(R.string.result_explained, result)
+                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                    }
                 })
         }
     }
