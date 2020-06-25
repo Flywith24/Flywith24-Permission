499aae954fce16b6e6149953c9ebec1992c40651
Flywith24 <fly_with24@163.com>
2020-06-25 21:44:43 +0800

    add comment in Java Demo

--- app/src/main/java/com/flywith24/permissiondemo/JavaActivity.java
+++ app/src/main/java/com/flywith24/permissiondemo/JavaActivity.java
@@ -2,6 +2,7 @@ package com.flywith24.permissiondemo;
 
 import android.Manifest;
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,7 +21,9 @@ import java.util.List;
  * description
  * Java Demo
  */
-class JavaActivity extends AppCompatActivity {
+public class JavaActivity extends AppCompatActivity {
+    private static final String TAG = "JavaActivity";
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,34 +31,34 @@ class JavaActivity extends AppCompatActivity {
         PermissionUtils.requestMultiplePermissions(this, new MultiPermissionResultListener() {
             @Override
             public void allGranted() {
-
+                Log.i(TAG, "allGranted: ");
             }
 
             @Override
             public void denied(List<String> list) {
-
+                Log.i(TAG, "denied: " + list.toString());
             }
 
             @Override
             public void explained(List<String> list) {
-
+                Log.i(TAG, "explained: " + list.toString());
             }
         }, permissions);
 
-        PermissionUtils.requestPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE, new PermissionResultListener() {
+        PermissionUtils.requestPermission(this, Manifest.permission.RECORD_AUDIO, new PermissionResultListener() {
             @Override
             public void granted(String permission) {
-
+                Log.i(TAG, "granted: ");
             }
 
             @Override
             public void denied(String permission) {
-
+                Log.i(TAG, "denied: ");
             }
 
             @Override
             public void explained(String permission) {
-
+                Log.i(TAG, "explained: ");
             }
         });
     }
