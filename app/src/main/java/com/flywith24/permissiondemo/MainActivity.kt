b0d46d7889e3ad80035612013dc1354fcbe7fdbb
Flywith24 <fly_with24@163.com>
2020-06-25 21:28:22 +0800

    use varg

--- app/src/main/java/com/flywith24/permissiondemo/MainActivity.kt
+++ app/src/main/java/com/flywith24/permissiondemo/MainActivity.kt
@@ -10,25 +10,28 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     fun click(view: View) {
-        requestMultiplePermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.CAMERA),
-                allGranted = {
-                    Snackbar.make(view, getString(R.string.result_granted_list), Snackbar.LENGTH_SHORT).show()
-                },
-                denied = {
-                    //防止太长体验不好，真实使用时无需做此操作
-                    val result = it.toString().replace("android.permission.", "")
-                    with(deniedResult) {
-                        text = getString(R.string.result_denied_list, result)
-                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
-                    }
-                },
-                explained = {
-                    //防止太长体验不好，真实使用时无需做此操作
-                    val result = it.toString().replace("android.permission.", "")
-                    with(explainedResult) {
-                        text = getString(R.string.result_explained_list, result)
-                        setTextColor(context.getThemeColor(R.attr.errorTextColor))
-                    }
-                })
+        requestMultiplePermissions(
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+            Manifest.permission.CAMERA,
+            allGranted = {
+                Snackbar.make(view, getString(R.string.result_granted_list), Snackbar.LENGTH_SHORT)
+                    .show()
+            },
+            denied = {
+                //防止太长体验不好，真实使用时无需做此操作
+                val result = it.toString().replace("android.permission.", "")
+                with(deniedResult) {
+                    text = getString(R.string.result_denied_list, result)
+                    setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                }
+            },
+            explained = {
+                //防止太长体验不好，真实使用时无需做此操作
+                val result = it.toString().replace("android.permission.", "")
+                with(explainedResult) {
+                    text = getString(R.string.result_explained_list, result)
+                    setTextColor(context.getThemeColor(R.attr.errorTextColor))
+                }
+            })
     }
 }
